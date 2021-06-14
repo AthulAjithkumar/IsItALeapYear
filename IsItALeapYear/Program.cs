5a3290dfb3bccd3756fb23e634023cb857b448ed
Abel Thomas <athoma12@confederationcollege.ca>
2021-06-13 23:15:01 -0400

    committing all files

---
+++ IsItALeapYear/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool IsItALeapYear(int var)
+        {
+
+            return false;
+        }
+
+        public bool Year_1996_Is_a_leap_year(int var1)
+        {
+
+            return true;
+        }
+
+        public bool Year_1900_is_not_a_leap_year(int var2)
+        {
+            if (var2 % 100 == 0)
+            {
+                return false;
+            }
+            else {
+                return true;
+            }
+            
+        }
+
+        public bool Year_2000_is_a_leap_year(int var3)
+        {
+            if (var3 % 400 == 0)
+            {
+                return false;
+            }
+            else if (var3 % 100 == 0)
+            {
+                return true;
+            }
+            else {
+                return false; 
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using IsItALeapYear;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void Year_2001_Is_not_a_leap_year()
+
+        {
+
+            // Arrange
+
+            var p = new Program();
+
+            var expected = false;
+
+            // Act
+
+            var actual = p.IsItALeapYear(2004);
+            var actual1 = p.Year_1996_Is_a_leap_year(1996);
+            var actual2 = p.Year_1900_is_not_a_leap_year(1990);
+            var actual3 = p.Year_2000_is_a_leap_year(2000);
+
+
+            // Assert
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
